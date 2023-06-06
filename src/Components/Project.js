7f4cce331275c6f4d17e9d5583b8a2e7240f115c
Josiah Storrie <jpstorrie@gmail.com>
2023-06-06 14:50:09 -0600

    chore: font change

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,15 +1,10 @@
 export default function Project({ project }) {
     return (
         <>
-            <h1>{project.name}</h1>
-            <h1>{project.description}</h1>
-            <h1>{project.languages}</h1>
+            <h1 className="font-['Comic_Sans_MS']">{project.name}</h1>
+            <h1 className="font-sans">{project.description}</h1>
+            <h1>{project.languages.join(", ")}</h1>
             <h1>{project.github}</h1>
         </>
     )
 }
-
-// "name": "KeepSight",
-// "description": "",
-// "languages": [],
-// "github": ""
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'sans': ['Comic Sans MS', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
