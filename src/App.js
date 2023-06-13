45458fb5476098913ef2290f62f60976b9da3770
Josiah Storrie <jpstorrie@gmail.com>
2023-06-12 19:40:34 -0600

    feat: style change and add about

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Project from "./Components/Project.js";
+import About from "./Components/about.js";
 import './App.css';
 import { useEffect, useState } from "react"
 
@@ -14,14 +15,14 @@ function App() {
 
   return (
     <div className="App">
-      <nav className="h-11 w-1/2 rounded-br-lg navbg">
+      <nav className="h-11 rounded-b-lg navbg">
         <ol className="flex p-2 justify-around">
           <li><a href="#">About Me</a></li>
           <li><a href="#">Technical Projects</a></li>
           <li><a href="#">Resume</a></li>
         </ol>
       </nav>
-
+      <About />
       <h1>projects</h1>
       {projectElements}
     </div>
--- src/Components/about.js
+++ src/Components/about.js
@@ -1,9 +1,15 @@
-export default function about(){
+export default function About(){
     return(
         <div>
-            <img src=""/>
-            <h1>Hi, I'm Josiah</h1>
-            <h1>About me here</h1>
+            <img className="rounded-full h-28"
+            src="https://media.licdn.com/dms/image/D5603AQHu7Kj5X0ljIA/profile-displayphoto-shrink_800_800/0/1674073759784?e=2147483647&v=beta&t=O1P4DrAuIakqohPgIUbXpay5MVEu4GOXvt24S92o1o4"/>
+            <h1>Hi, I'm Josiah!</h1>
+            <h1>As a young boy, I fell in love with all things computers... especially coding.
+            At the age of 9 I discovered Codecademy and getting me off of that website was near impossible. I devoutly enjoyed learning the basics of HTML, JavaScript, and CSS.
+            I continued to learn about web development throughout high school and while working in restaurants.
+            I then decided to continue my education by attending Flatiron School, a 3-month software engineering bootcamp,
+            where I learned a variety of skills like React, Ruby(on Rails), and a wide variety of other basic programming knowledge.
+            </h1>
         </div>
     )
 }
